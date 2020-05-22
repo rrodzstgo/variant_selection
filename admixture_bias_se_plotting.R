ab4a4cff12e283701ab23b93dc5bb59bcc4d2257
rrodzstgo <ronalderodriguez@gmail.com>
2020-05-22 14:40:26 -0400

    testing

--- admixture_bias_se_plotting.R
+++ admixture_bias_se_plotting.R
@@ -3,82 +3,106 @@
 
 
 admixture_bias_se_plotting <- function(bias_matrix,se_matrix,k,plot_title = NULL,plot_save_pattern=NULL,format=NULL,dpi=NULL){
-  
+
   #required packages
   require (dplyr)
   require (ggplot2)
   require(RColorBrewer)
-
+  
+  bias_matrix <- read.table("/shire/ronald/projects/gwas_1kg_targets/foundationone/ld02_maf05/foundationone_1kg_merged_ld02_maf05.8.Q_bias", quote="\"", comment.char="")
+  se_matrix <- foundationone_1kg_merged_ld02_maf05.8 <- read.table("/shire/ronald/projects/gwas_1kg_targets/foundationone/ld02_maf05/foundationone_1kg_merged_ld02_maf05.8.Q_se", quote="\"", comment.char="")
+  k <- 8
+  
   #plot default settings
-  plot_title <- ifelse(is.null(plot_title) == TRUE,"ADMIXTURE",clusters_title)
-  plot_save_pattern <- ifelse(is.null(plot_save_pattern) == TRUE,"admixture",plot_save_pattern)
-  dpi <- ifelse(is.null(dpi) == TRUE,300,dpi)
-  format <- ifelse(is.null(format) == TRUE,"tiff",format)
- 
+  plot_title <- "test"
+  plot_save_pattern <- "test"
+  dpi <-  300
+  format <- "tiff"
+  
   #load the 1000 Genomes sample information file and rename the first column as ID
-  sample_info_1kg <- read.csv("metadata_1kg_20130606_sample_info.csv")
-  colnames(sample_info_1kg)[1] <- "ID"
+  sample_info_1kg <- read.csv("1kG_labels.txt")
+  colnames(sample_info_1kg)[1] <- "population"
   
   #add ID column to the bias and standard error matrices
-  bias_matrix$ID <- sample_info_1kg$ID
-  se_matrix$ID <- sample_info_1kg$ID
-  
-  #add population column to the bias and standard error matrices
   bias_matrix$population <- sample_info_1kg$population
   se_matrix$population <- sample_info_1kg$population
   
   #add continental population column to the bias and standard error matrices
   
-  data_input_matrixes <- c("bias_matrix","se_matrix")
+  bias_matrix$population_continents[bias_matrix$population=="YRI"] <- 'AFR'
+  bias_matrix$population_continents[bias_matrix$population=="LWK"] <- 'AFR'
+  bias_matrix$population_continents[bias_matrix$population=="GWD"] <- 'AFR'
+  bias_matrix$population_continents[bias_matrix$population=="MSL"] <- 'AFR'
+  bias_matrix$population_continents[bias_matrix$population=="ESN"] <- 'AFR'
+  bias_matrix$population_continents[bias_matrix$population=="ASW"] <- 'AA'
+  bias_matrix$population_continents[bias_matrix$population=="ACB"] <- 'AA'
+  bias_matrix$population_continents[bias_matrix$population=="CEU"] <- 'EUR'
+  bias_matrix$population_continents[bias_matrix$population=="TSI"] <- 'EUR'
+  bias_matrix$population_continents[bias_matrix$population=="FIN"] <- 'EUR'
+  bias_matrix$population_continents[bias_matrix$population=="GBR"] <- 'EUR'
+  bias_matrix$population_continents[bias_matrix$population=="IBS"] <- 'EUR'
+  bias_matrix$population_continents[bias_matrix$population=="GIH"] <- 'SAS'
+  bias_matrix$population_continents[bias_matrix$population=="PJL"] <- 'SAS'
+  bias_matrix$population_continents[bias_matrix$population=="BEB"] <- 'SAS'
+  bias_matrix$population_continents[bias_matrix$population=="STU"] <- 'SAS'
+  bias_matrix$population_continents[bias_matrix$population=="ITU"] <- 'SAS'
+  bias_matrix$population_continents[bias_matrix$population=="CHB"] <- 'EAS'
+  bias_matrix$population_continents[bias_matrix$population=="JPT"] <- 'EAS'
+  bias_matrix$population_continents[bias_matrix$population=="CHS"] <- 'EAS'
+  bias_matrix$population_continents[bias_matrix$population=="CDX"] <- 'EAS'
+  bias_matrix$population_continents[bias_matrix$population=="KHV"] <- 'EAS'
+  bias_matrix$population_continents[bias_matrix$population=="MXL"] <- 'H/L'
+  bias_matrix$population_continents[bias_matrix$population=="PUR"] <- 'H/L'
+  bias_matrix$population_continents[bias_matrix$population=="CLM"] <- 'H/L'
+  bias_matrix$population_continents[bias_matrix$population=="PEL"] <- 'H/L'
   
-  for(matrix in data_input_matrixes){
-    matrix$population_continents[matrix$population=="YRI"] <- 'AFR'
-    matrix$population_continents[matrix$population=="LWK"] <- 'AFR'
-    matrix$population_continents[matrix$population=="GWD"] <- 'AFR'
-    matrix$population_continents[matrix$population=="MSL"] <- 'AFR'
-    matrix$population_continents[matrix$population=="ESN"] <- 'AFR'
-    matrix$population_continents[matrix$population=="ASW"] <- 'AA'
-    matrix$population_continents[matrix$population=="ACB"] <- 'AA'
-    matrix$population_continents[matrix$population=="CEU"] <- 'EUR'
-    matrix$population_continents[matrix$population=="TSI"] <- 'EUR'
-    matrix$population_continents[matrix$population=="FIN"] <- 'EUR'
-    matrix$population_continents[matrix$population=="GBR"] <- 'EUR'
-    matrix$population_continents[matrix$population=="IBS"] <- 'EUR'
-    matrix$population_continents[matrix$population=="GIH"] <- 'SAS'
-    matrix$population_continents[matrix$population=="PJL"] <- 'SAS'
-    matrix$population_continents[matrix$population=="BEB"] <- 'SAS'
-    matrix$population_continents[matrix$population=="STU"] <- 'SAS'
-    matrix$population_continents[matrix$population=="ITU"] <- 'SAS'
-    matrix$population_continents[matrix$population=="CHB"] <- 'EAS'
-    matrix$population_continents[matrix$population=="JPT"] <- 'EAS'
-    matrix$population_continents[matrix$population=="CHS"] <- 'EAS'
-    matrix$population_continents[matrix$population=="CDX"] <- 'EAS'
-    matrix$population_continents[matrix$population=="KHV"] <- 'EAS'
-    matrix$population_continents[matrix$population=="MXL"] <- 'H/L'
-    matrix$population_continents[matrix$population=="PUR"] <- 'H/L'
-    matrix$population_continents[matrix$population=="CLM"] <- 'H/L'
-    matrix$population_continents[matrix$population=="PEL"] <- 'H/L'
-  }
+  se_matrix$population_continents[se_matrix$population=="YRI"] <- 'AFR'
+  se_matrix$population_continents[se_matrix$population=="LWK"] <- 'AFR'
+  se_matrix$population_continents[se_matrix$population=="GWD"] <- 'AFR'
+  se_matrix$population_continents[se_matrix$population=="MSL"] <- 'AFR'
+  se_matrix$population_continents[se_matrix$population=="ESN"] <- 'AFR'
+  se_matrix$population_continents[se_matrix$population=="ASW"] <- 'AA'
+  se_matrix$population_continents[se_matrix$population=="ACB"] <- 'AA'
+  se_matrix$population_continents[se_matrix$population=="CEU"] <- 'EUR'
+  se_matrix$population_continents[se_matrix$population=="TSI"] <- 'EUR'
+  se_matrix$population_continents[se_matrix$population=="FIN"] <- 'EUR'
+  se_matrix$population_continents[se_matrix$population=="GBR"] <- 'EUR'
+  se_matrix$population_continents[se_matrix$population=="IBS"] <- 'EUR'
+  se_matrix$population_continents[se_matrix$population=="GIH"] <- 'SAS'
+  se_matrix$population_continents[se_matrix$population=="PJL"] <- 'SAS'
+  se_matrix$population_continents[se_matrix$population=="BEB"] <- 'SAS'
+  se_matrix$population_continents[se_matrix$population=="STU"] <- 'SAS'
+  se_matrix$population_continents[se_matrix$population=="ITU"] <- 'SAS'
+  se_matrix$population_continents[se_matrix$population=="CHB"] <- 'EAS'
+  se_matrix$population_continents[se_matrix$population=="JPT"] <- 'EAS'
+  se_matrix$population_continents[se_matrix$population=="CHS"] <- 'EAS'
+  se_matrix$population_continents[se_matrix$population=="CDX"] <- 'EAS'
+  se_matrix$population_continents[se_matrix$population=="KHV"] <- 'EAS'
+  se_matrix$population_continents[se_matrix$population=="MXL"] <- 'H/L'
+  se_matrix$population_continents[se_matrix$population=="PUR"] <- 'H/L'
+  se_matrix$population_continents[se_matrix$population=="CLM"] <- 'H/L'
+  se_matrix$population_continents[se_matrix$population=="PEL"] <- 'H/L'
   
   #load the order in which populations will be placed in the plots
   labels <- c("YRI","LWK","GWD","MSL","ESN","ASW","ACB","CEU","TSI","FIN","GBR","IBS","GIH","PJL","BEB","STU","ITU","CHB","JPT","CHS","CDX","KHV","MXL","PUR","CLM","PEL")
   
   #expand color palette to fit all the populations 
-  colourCount = length(unique(bias_matrix$population))
-  getPalette = colorRampPalette(brewer.pal(8, "Accent"))
+  colourCount <- length(unique(bias_matrix$population))
+  getPalette <- colorRampPalette(brewer.pal(8, "Accent"))
   
   #plot the bias_matrix and save it
   
   population_matrix_data <- c("population","population_continents")
   
-  for (k in k){
-    for (matrixes in data_input_matrixes){
-      for (population_columns in population_matrix_data){
-  ggplot(matrixes, aes(x = population, y=tsne2, color=population_columns))+ 
-    geom_point(alpha = 0.8) + theme_bw() + ggtitle(as.character(plot_title)) + scale_color_manual(values=getPalette(colourCount))
+  data_input_matrixes <- c(se_matrix,bias_matrix)
   
-  ggsave(filename =  paste(plot_save_pattern,k,matrixes,population_columns,format,sep = ""), device = format ,dpi = as.numeric(dpi))
+  
+  for (columns in colnames(bias_matrix)){
+    for (population_columns in population_matrix_data[1:k]){
+      ggplot(bias_matrix, aes(x = columns, color=population_columns))+ 
+        geom_dotplot() + theme_bw() + ggtitle(as.character(plot_title)) + scale_color_manual(values=getPalette(colourCount))
+      
+      ggsave(filename =  paste(plot_save_pattern,k,"bias_matrix",population_columns,".",format,sep = ""), device = format ,dpi = as.numeric(dpi))
     } 
-   }
   }
 }
